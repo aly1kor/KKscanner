8b8b3e99eafc2c8e092e3d5ed057e2428d2fd9f1
aly1kor <lobo_alwyn@yahoo.co.in>
2026-08-11 22:28:47 +0530

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -16,7 +16,7 @@ let checkinCounter = "Counter 1";
 // Controls
 // -----------------------------------------------------
 
-const SHOW_DIAGNOSTICS = true;
+const SHOW_DIAGNOSTICS = false;
 
 const topBar = document.getElementById("topBar");
 const backBtn = document.getElementById("backBtn");
